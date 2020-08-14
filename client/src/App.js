c0c49aa57a4603c16f050eef7e05622c612c7e27
七哥 <thomas@iMac.lan>
2020-08-14 20:35:00 +0800

    页面逻辑交互

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'
 import axios from 'axios'
 import { saveAs } from 'file-saver'
@@ -8,28 +8,39 @@ const styles = {
 }
 
 function App() {
-  const handleDownloadPdf = () => {
-    console.log(1)
+  const [info,setInfo] = useState({
+    name:'',
+    RecepitId :'',
+    Price1:'',
+    Price2:'',
+  })
+
+  const handleChange = ({target: {name,value}}) => {
+    const pd = Object.assign({},info,{[name]:value})
+    setInfo(pd)
+  }
+  const handleCreateDownloadPdf = () => {
+    console.log(info)
+    axios.post('/create-pdf',info)
   }
   return (
     <div className="App">
         <h1> PDF 配置下载</h1>
         <Row>
           <Col span={4} style={styles}>
-            
-            <Input placeholder="aaa"/>
+            <Input placeholder="Name" name="name" onChange={handleChange}/>
           </Col>
           <Col span={4} style={styles}> 
-            <Input placeholder="aaa"/>
+            <Input placeholder="Recepit ID " name="RecepitId" onChange={handleChange}/>
           </Col>
           <Col span={4} style={styles}> 
-            <Input placeholder="aaa"/>
+            <Input placeholder="Price1" name="Price1" onChange={handleChange}/>
           </Col>
           <Col span={4} style={styles}> 
-            <Input placeholder="aaa"/>
+            <Input placeholder="Price2" name="Price2" onChange={handleChange}/>
           </Col>
           <Button style={styles} 
-            onClick={handleDownloadPdf}
+            onClick={handleCreateDownloadPdf}
           >下载</Button>
         </Row>
     </div>
